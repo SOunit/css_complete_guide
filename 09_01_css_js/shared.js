198c6a75e5db4d4a95e9f7602fdee123de7d6443
SOunit <workbookwork7@gmail.com>
2021-07-24 00:41:51 -0700

    add close modal function to no-button, background

--- 09_01_css_js/shared.js
+++ 09_01_css_js/shared.js
@@ -3,9 +3,10 @@ console.log('hello from js!');
 // get access
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
+const modalNoButton = document.querySelector('.modal__action--negative');
+const selectPlanButtons = document.querySelectorAll('.plan button');
 
 // add event listener
-const selectPlanButtons = document.querySelectorAll('.plan button');
 console.log(selectPlanButtons);
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', () => {
@@ -14,3 +15,11 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
     modal.style.display = 'block';
   });
 }
+
+const closeModal = () => {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+};
+
+backdrop.addEventListener('click', closeModal);
+modalNoButton.addEventListener('click', closeModal);
