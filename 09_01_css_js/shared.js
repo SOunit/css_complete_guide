15b0d208466fae73a7e9f666be38a7502125fd7d
SOunit <workbookwork7@gmail.com>
2021-07-23 19:24:54 -0700

    add js to set display block to backdrop

---
+++ 09_01_css_js/shared.js
@@ -0,0 +1,9 @@
+console.log('hello from js!');
+
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
+
+console.log(backdrop);
+
+backdrop.style.display = 'block';
+modal.style.display = 'block';
