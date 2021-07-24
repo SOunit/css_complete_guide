c08f141e6ef3ee289539938065b25e5064ed818b
SOunit <workbookwork7@gmail.com>
2021-07-23 19:43:53 -0700

    add close modal function to no button

--- 09_02_practice/shared.js
+++ 09_02_practice/shared.js
@@ -3,6 +3,9 @@ console.log('hello from js!');
 // get access
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
+const noButton = document.querySelector(
+  '.modal__action.modal__action--negative'
+);
 
 // add event listener
 const selectPlanButtons = document.querySelectorAll('.plan button');
@@ -14,3 +17,8 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
     modal.style.display = 'block';
   });
 }
+
+noButton.addEventListener('click', () => {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+});
